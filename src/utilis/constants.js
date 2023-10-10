d91d495e82dd81fc5a0e39fe351433f96f2f5eeb
Akash Lande <akashlande97@gmail.com>
2023-10-10 21:40:59 +0530

    Fetch Movies

--- src/utilis/constants.js
+++ src/utilis/constants.js
@@ -2,4 +2,12 @@ export const LOGO ="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-8972
 
 export const Background_IMG ="https://assets.nflxext.com/ffe/siteui/vlv3/893a42ad-6a39-43c2-bbc1-a951ec64ed6d/1d86e0ac-428c-4dfa-9810-5251dbf446f8/IN-en-20231002-popsignuptwoweeks-perspective_alpha_website_large.jpg"
 
-export const User_Profile ="https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e"
+export const User_Profile ="https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+export const  options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNmVkYWEzMjUxMWZjYzZmYjg0NjgxMjQzNGY3NGEwNCIsInN1YiI6IjY1MjNjMzEyMGNiMzM1MTZmNjNhZTE0MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.p3itVZh2nWE8H6Si-jOCyGE4htCeNXmvqxBU65sJtP0'
+    }
+  };
